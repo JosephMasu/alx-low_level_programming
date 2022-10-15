1b0e45bf11b8b77d0068c28d03549710dbb3d8d2
JoeMasu <60499662+JoeMasu@users.noreply.github.com>
2022-10-15 06:31:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,3 @@
-/*
- * File: 1-last_digit.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -10,7 +5,6 @@
 /**
  * main - Prints the last digit of a randomly generated number
  *        and whether it is greater than 5, less than 6, or 0.
- *
  * Return: Always 0.
  */
 int main(void)
