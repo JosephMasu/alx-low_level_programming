3193bdceddb557cd4a2486cf1549f06748bd329b
JoeMasu <60499662+JoeMasu@users.noreply.github.com>
2022-10-17 16:02:02 -0700

    2-print_alphabet.c

---
+++ 2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main- Entry point
+*
+* Return: Always 0 (Success)
+*/
+int  main(void)
+{
+	int x;
+	char c;
+
+	for (x = 0; x < 26; x++)
+	{
+		c = 97 + x;
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
