cd03a2cb1aade629b4f39e66fbb124d4d205341f
JoeMasu <60499662+JoeMasu@users.noreply.github.com>
2022-10-28 14:10:42 -0700

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,46 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
+ * rot13 - encoder rot13
+ * @s: pointer to string params
  *
- * Return: A pointer to the encoded string.
+ * Return: *s
  */
-char *rot13(char *str)
-{
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-				'G', 'H', 'I', 'J', 'K', 'L',
-				'M', 'N', 'O', 'P', 'Q', 'R',
-				'S', 'T', 'U', 'V', 'W', 'X',
-				'Y', 'Z', 'a', 'b', 'c', 'd',
-				'e', 'f', 'g', 'h', 'i', 'j',
-				'k', 'l', 'm', 'n', 'o', 'p',
-				'q', 'r', 's', 't', 'u', 'v',
-				'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-				'T', 'U', 'V', 'W', 'X', 'Y',
-				'Z', 'A', 'B', 'C', 'D', 'E',
-				'F', 'G', 'H', 'I', 'J', 'K',
-				'L', 'M', 'n', 'o', 'p', 'q',
-				'r', 's', 't', 'u', 'v', 'w',
-				'x', 'y', 'z', 'a', 'b', 'c',
-				'd', 'e', 'f', 'g', 'h', 'i',
-				'j', 'k', 'l', 'm'};
 
-	while (str[indx1])
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+		for (j = 0; j < 52; j++)
 		{
-			if (str[indx1] == alphabet[indx2])
+			if (s[i] == data1[j])
 			{
-				str[indx1] = rot13key[indx2];
+				s[i] = datarot[j];
 				break;
 			}
 		}
-
-		indx1++;
 	}
-
-	return (str);
+	return (s);
 }
