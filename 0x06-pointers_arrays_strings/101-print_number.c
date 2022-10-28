6a8d7113dea9d48201227bbe9ff877dceb83f921
JoeMasu <60499662+JoeMasu@users.noreply.github.com>
2022-10-28 14:12:33 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,26 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _abs - returns absolute value of given int
- * @n: number for which we want to get the absolute value
- *
- * Return: absolute value of number n
- */
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
 
-int		_abs(int n)
+void print_number(int n)
 {
-	return ((n < 0) ? -n : n);
-}
+	unsigned int n1;
 
-/**
- * print_number - prints number on the standard output
- * @n: the number to be printed
- */
+	n1 = n;
 
-void	print_number(int n)
-{
 	if (n < 0)
+	{
 		_putchar('-');
+		n1 = -n;
+	}
 
-	if (n >= 10 || n <= -10)
-		print_number(_abs(n / 10));
-	_putchar(_abs(n % 10) + '0');
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
