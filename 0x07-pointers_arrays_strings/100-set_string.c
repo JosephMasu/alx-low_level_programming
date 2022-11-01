e611ef27fedaa4a30a012d0bd7732602b6c14c68
JoeMasu <60499662+JoeMasu@users.noreply.github.com>
2022-11-01 14:31:10 -0700

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,29 +1,13 @@
-#include "main.h"                                                                                                                  
-#include <stdio.h>                                                                                                                 
-                                                                                                                                   
-/**                                                                                                                                
- * print_diagsums - Prints the sum of the two diagonals                                                                            
- *                  of a square matrix of integers.                                                                                
- * @a: The matrix of integers.                                                                                                     
- * @size: The size of the matrix.                                                                                                  
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ *@to: string
+ *Return: void
  */
-void print_diagsums(int *a, int size)                                                                                              
-{                                                                                                                                  
-        int index, sum1 = 0, sum2 = 0;                                                                                             
-                                                                                                                                   
-        for (index = 0; index < size; index++)                                                                                     
-        {                                                                                                                          
-                sum1 += a[index];                                                                                                  
-                a += size;                                                                                                         
-        }                                                                                                                          
-                                                                                                                                   
-        a -= size;                                                                                                                 
-                                                                                                                                   
-        for (index = 0; index < size; index++)                                                                                     
-        {                                                                                                                          
-                sum2 += a[index];                                                                                                  
-                a -= size;                                                                                                         
-        }                                                                                                                          
-                                                                                                                                   
-        printf("%d, %d\n", sum1, sum2);                                                                                            
+
+void set_string(char **s, char *to)
+{
+	*s = to;
 }
